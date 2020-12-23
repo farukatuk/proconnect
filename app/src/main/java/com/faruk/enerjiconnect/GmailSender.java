375d4cdef61ef57a23fc36e07e83b4edd107b6a0
Faruk Atuk <farukatuk@Beril-MacBook-Air.local>
2020-12-23 18:23:59 +0300

    Initial commit

---
+++ app/src/main/java/com/faruk/enerjiconnect/GmailSender.java
@@ -0,0 +1,86 @@
+package com.faruk.enerjiconnect;
+
+import java.io.File;
+import java.security.Security;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class GmailSender extends Authenticator {
+    private String mailhost = "mail.enerjiburo.com";
+    private String user;
+    private String password;
+    private Session session;
+
+    static {
+        Security.addProvider(new com.faruk.enerjiconnect.JSSEProvider());
+    }
+
+    public GmailSender(String user, String password) {
+        this.user = user;
+        this.password = password;
+
+        Properties props = new Properties();
+        props.setProperty("mail.transport.protocol", "smtp");
+        props.setProperty("mail.host", mailhost);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "587");
+        //props.put("mail.smtp.socketFactory.port", "465");
+        //props.put("mail.smtp.socketFactory.class",
+        //         "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.quitwait", "false");
+
+        session = Session.getDefaultInstance(props, this);
+    }
+
+    protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(user, password);
+    }
+
+    public synchronized void sendMail(String subject, String body, String sender, String recipients, File attachment) throws Exception {
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setSender(new InternetAddress(sender));
+            message.setSubject(subject);
+            message.setFrom(new InternetAddress("teknikservis@enerjiburo.com"));
+
+            MimeBodyPart mbp1 = new MimeBodyPart();
+            mbp1.setText(body);
+
+            MimeBodyPart mbp2 = new MimeBodyPart();
+            FileDataSource fds = new FileDataSource(attachment);
+            mbp2.setDataHandler(new DataHandler(fds));
+            mbp2.setFileName(fds.getName());
+
+            Multipart mp = new MimeMultipart();
+            mp.addBodyPart(mbp1);
+            mp.addBodyPart(mbp2);
+
+            message.setContent(mp);
+
+            if (recipients.indexOf(',') > 0)
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
+            else
+                message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients));
+            message.addRecipient(Message.RecipientType.CC, new InternetAddress(
+                    "ousta@enerjiburo.com"));
+
+            Transport.send(message);
+        } catch (Exception e) {
+
+        }
+
+    }
+}
